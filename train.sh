74b25237cc31f82ddf07cd3fa32cf31c2abb7ad8
Yush Hemkeshsing Rajcoomar <46701417+YushRajcoomar@users.noreply.github.com>
2024-02-13 10:31:56 -0500

    Update train.sh
    made shell script linux compatible for notebooks

--- train.sh
+++ train.sh
@@ -1,14 +1,14 @@
-BASEDIR="C:\Users\yush2\OneDrive\Desktop\kreol"
-FAIRSEQ=${BASEDIR}\\fairseq\\fairseq_cli
-PRETRAIN=${BASEDIR}\\mbart50.ft.nn\\mbart50.ft.nn
-langs=${BASEDIR}\\mbart50.ft.nn\\mbart50.ft.nn\\ML50_langs.txt
+BASEDIR=$(pwd)
+FAIRSEQ=${BASEDIR}/fairseq/fairseq_cli
+PRETRAIN=${BASEDIR}/mbart50.ft.nn/
+langs=${BASEDIR}/mbart50.ft.nn/ML50_langs.txt
 SRC=en
 TGT=cr
 NAME=en-cr
-DATADIR=${BASEDIR}\\postprocessed\\${NAME}
-SAVEDIR=${BASEDIR}\\checkpoint
+DATADIR=${BASEDIR}/postprocessed/${NAME}
+SAVEDIR=${BASEDIR}/checkpoint
 
-python ${FAIRSEQ}\\train.py ${DATADIR}  --encoder-normalize-before --decoder-normalize-before  --arch mbart_large \
+python ${FAIRSEQ}/train.py ${DATADIR}  --encoder-normalize-before --decoder-normalize-before  --arch mbart_large \
 --task translation_from_pretrained_bart  --source-lang ${SRC} --target-lang ${TGT} --criterion label_smoothed_cross_entropy \
 --label-smoothing 0.2  --dataset-impl mmap --optimizer adam --adam-eps 1e-06 --adam-betas '(0.9, 0.98)' \
 --lr-scheduler inverse_sqrt --lr 3e-05 --warmup-updates 2500 --max-update 40000 --dropout 0.3 \
