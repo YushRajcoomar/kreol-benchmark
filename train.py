d9ae6afca94660683d6ab54bc8dc84380d6eafa3
DeleMike <akindelemichael65@gmail.com>
2024-03-02 20:26:53 +0000

    ugh

--- train.py
+++ train.py
@@ -1,6 +1,10 @@
 from pathlib import Path
 from tokenizers.implementations import SentencePieceBPETokenizer
 import pandas as pd
+import numpy as np
+import evaluate
+
+
 from transformers import MBart50Tokenizer
 import torch
 from transformers import MBartConfig, MBartForConditionalGeneration, DataCollatorForSeq2Seq, Seq2SeqTrainingArguments #, Seq2SeqTrainer
@@ -17,39 +21,57 @@ TOKENIZER_VOCABULARY = 20000  # Total number of unique subwords the tokenizer ca
 os.environ["WANDB_PROJECT"] = "Kreol - NMT"  # name your W&B project
 os.environ["WANDB_LOG_MODEL"] = "checkpoint" 
 
+def compute_metrics(eval_preds):
+    metric = evaluate.load("sacrebleu")
+    preds, labels = eval_preds
+    # In case the model returns more than the prediction logits
+    if isinstance(preds, tuple):
+        preds = preds[0]
+
+    decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
+
+    # Replace -100s in the labels as we can't decode them
+    labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+    decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+
+    # Some simple post-processing
+    decoded_preds = [pred.strip() for pred in decoded_preds]
+    decoded_labels = [[label.strip()] for label in decoded_labels]
+
+    result = metric.compute(predictions=decoded_preds, references=decoded_labels)
+    return {"bleu": result["score"]}
+
 tokenizer = MBart50Tokenizer.from_pretrained("/mnt/disk/yrajcoomar/kreol-benchmark/pipelines/tok",max_len=256)
 
 dataset = load_dataset(
     "json",
-    data_files={'train':'/mnt/disk/yrajcoomar/kreol-benchmark/data/lang_data/en-cren-cr_train.jsonl','test':'/mnt/disk/yrajcoomar/kreol-benchmark/data/lang_data/en-cren-cr_test.jsonl',
-                'val':'/mnt/disk/yrajcoomar/kreol-benchmark/data/lang_data/en-cren-cr_dev.jsonl'}
+    data_files={'train':'/mnt/disk/yrajcoomar/kreol-benchmark/data/lang_data/en-cr/en-cr_train.jsonl','test':'/mnt/disk/yrajcoomar/kreol-benchmark/data/lang_data/en-cr/en-cr_test.jsonl',
+                'val':'/mnt/disk/yrajcoomar/kreol-benchmark/data/lang_data/en-cr/en-cr_dev.jsonl'}
 )
 
 tokenize_fn = preprocess_function(tokenizer)
 dataset = dataset.map(tokenize_fn, batched=True)
 train_dataset, test_dataset, val_dataset = dataset.values()
 
-
 print("CUDA:", torch.cuda.is_available())
 
 config = MBartConfig(vocab_size=TOKENIZER_VOCABULARY,max_position_embeddings=512,forced_eos_token_id=2)
 model = MBartForConditionalGeneration(config)
-
 
 collator = DataCollatorForSeq2Seq(
     tokenizer=tokenizer, model=model
 )
 
 training_args = Seq2SeqTrainingArguments(
-    output_dir='./checkpoint',
-    num_train_epochs=200,
-    per_gpu_train_batch_size=32,
-    per_gpu_eval_batch_size=4,
+    output_dir='./checkpoint_tests',
+    num_train_epochs=4,
+    per_gpu_train_batch_size=2,
+    per_gpu_eval_batch_size=1,
     prediction_loss_only=True,
     report_to="wandb",
-    run_name = "<insert name>"
-)
+    run_name = "test_run_bleu",
 
+)
 
 trainer = Seq2SeqTrainer(
     model=model,
@@ -57,6 +79,8 @@ trainer = Seq2SeqTrainer(
     data_collator=collator,
     train_dataset=train_dataset,
     eval_dataset=test_dataset,
+    # tokenizer=tokenizer,
+    # compute_metrics=compute_metrics
 )
 
 trainer.train()
